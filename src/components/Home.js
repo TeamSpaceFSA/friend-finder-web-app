5ea69c0b6b6081c141c4864206e4e1d8bf0343f0
Jenny Wu <jennywuzheng@gmail.com>
2022-12-02 12:31:49 -0500

    fix

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,7 +10,7 @@ const Home = () => {
      }
     return(
         <div>Welcome!
-            <button onClick={()=>logout}>Logout</button>
+            <button onClick={logout}>Logout</button>
         </div>
     )
 }
