a0c5c25e5eda53761ee4572cd3e2e0d4efc9f05a
Jenny Wu <jennywuzheng@gmail.com>
2022-12-02 11:29:47 -0500

    added components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { Home, ProfileSetup, SignIn, SignUp } from './components/index'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path = "/" element={<SignUp/>}/>
+        <Route path = "/signin" element={<SignIn/>}/>
+        <Route path = "/home" element={<Home/>}/>
+        <Route path = "/setupprofile" element={<ProfileSetup/>}/>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as SignUp} from './SignUp'
+export { default as SignIn} from './SignIn'
+export { default as Home } from './Home'
+export { default as ProfileSetup } from './ProfileSetup'
