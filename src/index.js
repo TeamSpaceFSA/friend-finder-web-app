305d711fdfe5de1e4b4eb01d03b8db555373f074
Harry <harry.huang887@gmail.com>
2022-12-05 15:53:15 -0500

    fixed store location

--- src/index.js
+++ src/index.js
@@ -2,12 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './app/index.css';
 import App from './app/App';
+import store from './app/store.js'
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
 import reportWebVitals from './app/reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Router>
   </React.StrictMode>
 );
 
