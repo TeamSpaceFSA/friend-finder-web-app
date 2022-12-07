0539162be646bb4185b656fb7a26b03454722393
Jacob Saddler <jacobasaddler@gmail.com>
2022-12-07 14:46:29 -0600

    Props are successfully passed to SingleEventView, comments added for clarification

--- src/components/Events/SingleEventView.js
+++ src/components/Events/SingleEventView.js
@@ -1,11 +1,18 @@
 import React from "react";
+import {useLocation} from 'react-router-dom';
 
-const SingleEventView = ({singleEvent}) => {
+const SingleEventView = () => {
+    //useLocation is taking the props/state we passed in from our navigate function (HomeMap.js: line 39)
+    const location = useLocation()
+    //location.state holds all of our single event data, therefore we define it to make it easier to refer to.
+    const event = location.state
+    console.log("This is the state we have passed in from the Marker we have clicked", location.state)
+
     
-    console.log("This is our passed prop singleEvent:", singleEvent)
     return(
         <div>
             <h1>This is the single event view</h1>
+            <h1>Name: {event.name}</h1>
         </div>
     )
 }
--- src/components/MapComponents/HomeMap.js
+++ src/components/MapComponents/HomeMap.js
@@ -9,16 +9,21 @@ import { db } from "../../app/FirebaseConfig"
 import { onSnapshot, query, collection, doc, getDocs, documentId } from "firebase/firestore";
 
 const HomeMap = () => {
+  //Need 'libraries' for Google Maps API.
   const libraries = ["places"]
   const navigate = useNavigate()
+  
+  //This is the 'events' state so we can change/modify our state of All Events.
   const [events, setEvents] = useState([])
 
+  //This is simply something we need for our maps to show up. Can't really explain how it works besides the fact that
+  //we need our API Key for Google Maps to be somewhere on the page.
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: "AIzaSyBGp2IIHledlGmrdnK8M7x7QMn3GE6uiIg",
     libraries: libraries,
   })
 
-  //Fetching all event data
+  //Fetching all event data from Firebase
   const fetchEvents = async () => {
     try {
       const q = query(collection(db, "events"))
@@ -34,18 +39,22 @@ const HomeMap = () => {
     }
   }
 
+  //This is the function that routes us to our SingleEventView.js component page.
   const viewSingleEvent = () => {
-    // e.preventDefault()
-    navigate("/singleEventView", {singleEvent: selectedMarker})
+    // Here, we pass the variable 'selectedMarker' as a value for our key 'state' so our SingleEventView 
+    // component recognizes which event we are referring to.
+    navigate("/singleEventView", {state: selectedMarker})
     console.log("Here is the selectedMarker", selectedMarker)
   }
 
-
+  //Fetch all events everytime the page is loaded.
   useEffect(() => {
     fetchEvents()
   }, [])
 
-  const [selected, setSelected] = useState(null);
+  
+  // const [selected, setSelected] = useState(null); //This was the state for our SearchBar autocomplete functionality. Since we took the searchbar out of the Homepage for right now,
+  // I've commented it out as well as the <PlacesAutoComplete /> div & the PlacesAutoComplete function on line 84. Commented it out for now in case we want to bring it back later.
   const [markers, setMarkers] = useState([]);
   const [ selectedMarker, setSelectedMarker ] = useState("")
 
@@ -57,7 +66,7 @@ const HomeMap = () => {
     <>
       <div>Available Events</div>
       <div className="places-container">
-        <PlacesAutocomplete setSelected={setSelected} />
+        {/* <PlacesAutocomplete setSelected={setSelected} /> */}
       </div>
       <GoogleMap zoom={10} center={{ lat: 40.7580, lng: -73.9855 }} mapContainerClassName="map-container">
         {markers.map(marker => <MarkerF key={marker.time.toISOString()} position={{ lat: marker.lat, lng: marker.lng }} icon="https://i.imgur.com/OX3qSvl.png" />)}
@@ -77,45 +86,45 @@ const HomeMap = () => {
 }
 
 //This creates the autocomplete-address-search-bar that we can use to set locations. 
-const PlacesAutocomplete = ({ setSelected }) => {
-  const {
-    ready,
-    value,
-    setValue,
-    suggestions: { status, data },
-    clearSuggestions,
-  } = usePlacesAutocomplete();
+// const PlacesAutocomplete = ({ setSelected }) => {
+//   const {
+//     ready,
+//     value,
+//     setValue,
+//     suggestions: { status, data },
+//     clearSuggestions,
+//   } = usePlacesAutocomplete();
 
-  //This handles the event of clicking on the address.
-  const handleSelect = async (address) => {
-    setValue(address, false);
-    clearSuggestions();
+//   //This handles the event of clicking on the address.
+//   const handleSelect = async (address) => {
+//     setValue(address, false);
+//     clearSuggestions();
 
-    const results = await getGeocode({ address });
-    const { lat, lng } = await getLatLng(results[0]);
-    setSelected({ lat, lng });
-  };
+//     const results = await getGeocode({ address });
+//     const { lat, lng } = await getLatLng(results[0]);
+//     setSelected({ lat, lng });
+//   };
 
-  return (
-    <Combobox onSelect={handleSelect}>
-      <ComboboxInput
-        value={value}
-        onChange={(e) => setValue(e.target.value)}
-        disabled={!ready}
-        className="combobox-input"
-        placeholder="Search an address"
-      />
-      <ComboboxPopover>
-        <ComboboxList>
-          {status === "OK" &&
-            data.map(({ place_id, description }) => (
-              <ComboboxOption key={place_id} value={description} />
-            ))}
-        </ComboboxList>
-      </ComboboxPopover>
-    </Combobox>
-  );
-};
+//   return (
+//     <Combobox onSelect={handleSelect}>
+//       <ComboboxInput
+//         value={value}
+//         onChange={(e) => setValue(e.target.value)}
+//         disabled={!ready}
+//         className="combobox-input"
+//         placeholder="Search an address"
+//       />
+//       <ComboboxPopover>
+//         <ComboboxList>
+//           {status === "OK" &&
+//             data.map(({ place_id, description }) => (
+//               <ComboboxOption key={place_id} value={description} />
+//             ))}
+//         </ComboboxList>
+//       </ComboboxPopover>
+//     </Combobox>
+//   );
+// };
 
 
 
