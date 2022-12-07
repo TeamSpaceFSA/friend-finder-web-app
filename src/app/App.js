25b6599247553182e91d4470ec3f237521d836ba
Andrea 'Dre' Arnold <a.arnold_08@yahoo.com>
2022-12-07 11:02:15 -0500

    minor changes

--- src/app/App.js
+++ src/app/App.js
@@ -1,15 +1,11 @@
 import './App.css';
 import React from "react";
 import AppRoutes from "./AppRoutes";
-import { HomeMap } from "../components/index";
-import { Footer } from "../components/index";
 
 const App = () => {
   return (
     <div>
       <AppRoutes />
-      {/* <HomeMap /> */}
-      <Footer />
     </div>
   );
 };
--- src/app/AppRoutes.js
+++ src/app/AppRoutes.js
@@ -1,36 +1,63 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import { ProfileSetup, SignIn, SignUp, Error, Resetpassword, Settings, Help, Suggestions, About, Profile, EditProfile, CreateEventMap, FriendsList, HomeMap, CreateEventForm, EventList } from '../components/index'
-import { auth } from './FirebaseConfig'
-import { useAuthState } from "react-firebase-hooks/auth"
-import { ToastContainer } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  ProfileSetup,
+  SignIn,
+  SignUp,
+  Error,
+  Resetpassword,
+  Settings,
+  Help,
+  Suggestions,
+  About,
+  Profile,
+  EditProfile,
+  CreateEventMap,
+  FriendsList,
+  HomeMap,
+  CreateEventForm,
+  EventList,
+  Footer,
+} from "../components/index";
+import { auth } from "./FirebaseConfig";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function AppRoutes() {
-  const [user] = useAuthState(auth)
+  const [user] = useAuthState(auth);
   return (
     <>
       {user ? (
-        <Routes>
-          <Route path="/" element={<SignUp />} />
-          <Route path="/signin" element={<SignIn />} />
-          <Route path="/home" element={<HomeMap />} />
-          <Route path="/setupprofile" element={<ProfileSetup />} />
-          <Route path="/resetpassword" element={<Resetpassword />} />
-          <Route path="/settings" element={<Settings/>}/>
-          <Route path="/help" element={<Help/>}/>
-          <Route path="/suggestions" element={<Suggestions/>}/>
-          <Route path="/about" element={<About/>}/>
-          <Route path="/profile" element={<Profile/>}/>
-          <Route path="/profile/editprofile" element={<EditProfile/>}/>
-          <Route path="/profile/friendslist" element={<FriendsList/>}/>
-          <Route path="/profile/createEventMap" element={<CreateEventMap/>}/>
-          <Route path="/profile/createEventForm" element={<CreateEventForm/>}/>   
-          <Route path="/profile/eventList" element={<EventList/>}/>         
-      
-          <Route path="*" element={<Error />} />
-        </Routes>
-        ) : (
+        <>
+          <Routes>
+            <Route path="/" element={<SignUp />} />
+            <Route path="/signin" element={<SignIn />} />
+            <Route path="/home" element={<HomeMap />} />
+            <Route path="/setupprofile" element={<ProfileSetup />} />
+            <Route path="/resetpassword" element={<Resetpassword />} />
+            <Route path="/settings" element={<Settings />} />
+            <Route path="/help" element={<Help />} />
+            <Route path="/suggestions" element={<Suggestions />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/profile/editprofile" element={<EditProfile />} />
+            <Route path="/profile/friendslist" element={<FriendsList />} />
+            <Route
+              path="/profile/createEventMap"
+              element={<CreateEventMap />}
+            />
+            <Route
+              path="/profile/createEventForm"
+              element={<CreateEventForm />}
+            />
+            <Route path="/profile/eventList" element={<EventList />} />
+
+            <Route path="*" element={<Error />} />
+          </Routes>
+          <Footer />
+        </>
+      ) : (
         <Routes>
           <Route path="/" element={<SignUp />} />
           <Route path="/signin" element={<SignIn />} />
@@ -38,16 +65,18 @@ function AppRoutes() {
           <Route path="*" element={<Error />} />
         </Routes>
       )}
-    <ToastContainer position="top-right"
-autoClose={5000}
-hideProgressBar={false}
-newestOnTop={false}
-closeOnClick
-rtl={false}
-pauseOnFocusLoss
-draggable
-pauseOnHover
-theme="light"/>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
     </>
   );
 }
