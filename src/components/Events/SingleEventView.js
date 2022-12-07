ea30d4b54cec9fea466f7865548313d1ac5d5f17
Jenny Wu <jennywuzheng@gmail.com>
2022-12-07 16:20:02 -0500

    added to single event view and friend list

--- src/components/Events/SingleEventView.js
+++ src/components/Events/SingleEventView.js
@@ -7,12 +7,18 @@ const SingleEventView = () => {
     //location.state holds all of our single event data, therefore we define it to make it easier to refer to.
     const event = location.state
     console.log("This is the state we have passed in from the Marker we have clicked", location.state)
-
+    const { name, category, headcount, startTime, endTime, description } = event
     
     return(
         <div>
-            <h1>This is the single event view</h1>
-            <h1>Name: {event.name}</h1>
+            <h1>Event: {name}</h1>
+            <h2>Description:{description}</h2>
+            <h2> # of People: {headcount}</h2>
+            <h2>Start:{startTime}</h2>
+            <h2>End:{endTime}</h2>
+            <h2>Activity:{category ? category.map(cat => (
+                <p key={cat}>{cat}</p>
+            )): <p>n/a</p>}</h2>
         </div>
     )
 }
